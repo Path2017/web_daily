ebff5ffbaae0386d812f4d621c0f2819880074b3
pathliu <path2017@126.com>
2018-05-04 15:25:20 +0800

    submit amd.js

--- AMD/util.js
+++ AMD/util.js
@@ -2,10 +2,10 @@ define(function() {
   var util = {
     getFormatDate: function(date,type){
       if(type === 1){
-        return '2018-5-4'
+        return 'date1'
       }
       if(type === 2){
-        return '2018年5月4日'
+        return 'date2'
       }
     }
   }
