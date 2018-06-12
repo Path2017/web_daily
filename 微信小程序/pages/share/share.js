bebcd8e21bbb9983cb6b98526a94b27342b662af
pathliu <path2017@126.com>
2018-06-12 17:13:37 +0800

    修改微信小程序

--- 微信小程序/pages/share/share.js
+++ 微信小程序/pages/share/share.js
@@ -7,17 +7,38 @@ Page({
   data: {
     shareImage: '',//生成图片的地址
     showSharePic: false,// 显示分享按钮
+    qrCodePath: '',
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
+    let that = this
+    wx.getImageInfo({
+      src: '../../images/wechat.jpg',//服务器返回的带参数的小程序码地址
+      success: function (res) {
+        //res.path是网络图片的本地地址
+        that.qrCodePath = res.path;
+        console.log(that.qrCodePath)
+      },
+      fail: function (res) {
+        //失败回调
+      }
+    });
     // 屏幕宽高
     let windowWidth = wx.getSystemInfoSync().windowWidth;
     let windowHeight = wx.getSystemInfoSync().windowHeight;
-    const ctx = wx.createCanvasContext('shareCanvas');
-    ctx.setFontSize(18);
+    console.log(windowWidth)
+    //  绘制文字区域背景 - begin
+    const ctx = wx.createCanvasContext('shareCanvas'); //画完整屏
+    ctx.fillStyle = "#f6f6f6";
+    ctx.fillRect(0, 0, windowWidth, windowHeight);
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, 350, 524);
+    //  绘制文字区域背景 - end
+    ctx.setFontSize(16);
     ctx.fillStyle = "#858585";
+    ctx.setTextAlign('justify');
     ctx.lineWidth = 1;
     var str = "事件分为冒泡事件和非冒泡事件： 冒泡事件：当一个组件上的事件被触发后，该事件会向父节点传递";
     var titleHeight = 55; // 标题的高度
@@ -25,7 +46,7 @@ Page({
     var initHeight = 35;//绘制字体距离canvas顶部初始的高度
 
     // 标题border-bottom 线距顶部距离
-    titleHeight = this.drawText(ctx, str, initHeight, titleHeight, canvasWidth);
+    titleHeight = this.drawText(ctx, str, initHeight, titleHeight, canvasWidth, 24);
     ctx.moveTo(15, titleHeight)
     ctx.lineTo(325, titleHeight)
     ctx.stroke() //绘制已定义的路径
@@ -33,8 +54,11 @@ Page({
 
     // 内容部分
     var detstr = "absolute定位是布局中最常用到的定位，其生成的位置是相对于带有position属性的父（父...)级来定位；如果父级都没有position属性，则相对于document来定位；使用absolute定位后，定位元素是脱离文档流的，这时候父级会检测不到定位元素的宽高。inline元素使用absolute定位之后，可以对其设置宽高；元素是不可以同时使用绝对定位和浮动的。";
-    titleHeight = this.drawText(ctx, detstr, titleHeight, titleHeight, canvasWidth);
+    titleHeight = this.drawText(ctx, detstr, titleHeight, titleHeight, canvasWidth, 24);
     console.log(titleHeight)
+    var imgurl = '../../images/wechat.jpg';
+    console.log(imgurl)
+    ctx.drawImage(imgurl, 125, 400, 100, 100);
     ctx.draw();
 
     // 延时生成图片
@@ -65,18 +89,20 @@ Page({
  * @param str 文本内容
  * @param initHeight 文本绘制的初始高度
  * @param titleHeight 绘制文本的高度
+ * @param canvasWidth 绘制文本区域画布宽度
+ * @param lineHeight 文字行高
  */
-  drawText: function (ctx, str, initHeight, titleHeight, canvasWidth) {
+  drawText: function (ctx, str, initHeight, titleHeight, canvasWidth, lineHeight) {
     var lineWidth = 0;
     var lastSubStrIndex = 0; //每次开始截取的字符串的索引
     for (let i = 0; i < str.length; i++) {
       lineWidth += ctx.measureText(str[i]).width;
       if (lineWidth > canvasWidth) {
         ctx.fillText(str.substring(lastSubStrIndex, i), 15, initHeight);//绘制截取部分
-        initHeight += 30;//20为字体的高度
+        initHeight += lineHeight;//20为字体的高度
         lineWidth = 0;
         lastSubStrIndex = i;
-        titleHeight += 30;
+        titleHeight += lineHeight;
       }
       if (i == str.length - 1) {//绘制剩余部分
         ctx.fillText(str.substring(lastSubStrIndex, i + 1), 15, initHeight);
